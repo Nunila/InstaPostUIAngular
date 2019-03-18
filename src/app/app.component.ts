38140c1cf540bddfd50994337bd905c5643aa0b3
nuniladavila <nunila.davila@upr.edu>
2019-03-18 02:41:05 -0400

    Added http management example for chats.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) { }
+
   title = 'UIInstaPost';
+  mainUrl = `http://localhost:5000/InstaPost`;
+
+  test() {
+    const url =  this.mainUrl + `/chats`;
+    const headersDict = {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache'
+    };
+    const requestOptions = {
+      headers: new HttpHeaders(headersDict)
+    };
+    this.http.get(url, requestOptions)
+      .subscribe(data => {
+      console.log(data);
+      this.title = data.toString();
+    });
+  }
 }
